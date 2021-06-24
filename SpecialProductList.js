6da0ae8074ef5276404bb7908ea5fe1f8382bc4a
Jeff Jiang <jjiang4566@gmail.com>
2021-06-23 19:49:45 -0500

    add the Special Product List problem

---
+++ SpecialProductList.js
@@ -0,0 +1,35 @@
+/*
+problem form https://binarysearch.com/problems/Special-Product-List
+
+Special Product List
+
+Given a list of integers nums, return a new list such that each element at index i of the new list is the product of all the numbers in the original list except the one at i.
+
+Constraints
+
+2 ≤ n ≤ 100,000 where n is the length of nums
+
+
+Example 1
+Input
+nums = [1, 2, 3, 4, 5]
+Output
+[120, 60, 40, 30, 24]
+Explanation
+120 = 2 * 3 * 4 * 5, 60 = 1 * 3 * 4 * 5, and so on.
+
+Example 2
+Input
+nums = [3, 2, 1]
+Output
+[2, 3, 6]
+
+
+I: array of numbers
+O: a new list with product of all other index
+E: a empty array
+C: 2 ≤ n ≤ 100,000 where n is the length of nums
+
+Note:  Do this without using division.
+
+*/

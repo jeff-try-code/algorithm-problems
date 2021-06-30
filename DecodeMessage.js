77116edc134604caff9ed5035178933f7ec16646
Jeff Jiang <jjiang4566@gmail.com>
2021-06-30 10:27:59 -0500

    add problem decode message

---
+++ DecodeMessage.js
@@ -0,0 +1,50 @@
+/*
+problem form https://binarysearch.com/problems/Decode-Message
+
+Decode Message
+
+Given the mapping "a" = 1, "b" = 2, ... "z" = 26, and an encoded message message (as a string), count the number of ways it can be decoded.
+
+Constraints
+
+n ≤ 100,000 where n is the length of message
+
+Example 1
+Input
+message = "111"
+Output
+3
+Explanation
+This can be decoded 3 ways: aaa, ak, and ka.
+
+Example 2
+Input
+message = "8"
+Output
+1
+Explanation
+This can be only decoded one way, as h.
+
+Example 3
+Input
+message = "12"
+Output
+2
+Explanation
+This can be decoded 2 ways: ab or l.
+
+Example 4
+Input
+message = "60"
+Output
+0
+Explanation
+There's no way to decode this message.
+
+Example 5
+Input
+message = "0012"
+Output
+0
+
+*/

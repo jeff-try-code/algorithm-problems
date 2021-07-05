7914927927257726c0451a790fd2785be060febc
Jeff Jiang <72947113+jeff-try-code@users.noreply.github.com>
2021-07-05 15:11:36 -0500

    Time: 160 ms (98.58%), Space: 47.5 MB (98.81%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let s = String(x)
+    for(let i = 0; i < s.length / 2; i++) {
+        if(s[i] !== s[s.length - i - 1]) return false
+    }
+    return true
+};
